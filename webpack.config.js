63685571a2a4caf4e019b801a059ccfa03f4cf6c
jdmm0718 <juandavidmm0718@gmail.com>
2022-04-26 21:50:06 -0500

    style

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -10,22 +11,30 @@ module.exports = {
   resolve: {
     extensions: ['.js']
   },
-  module: { 
+  module: {
     rules: [
-    {
-      test: /\.m?js$/,
-      exclude: /node_modules/,
-      use: {
-        loader:'babel-loader'
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css|.styl$/i,
+        use: [MiniCssExtractPlugin.loader,
+          'css-loader',
+          'stylus-loader'
+        ],
       }
-  }
-]
-          },
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       inject: true,
       template: './public/index.html',
       filename: './index.html'
-    })
-  ]        
+    }),
+    new MiniCssExtractPlugin(),
+  ]
 }
