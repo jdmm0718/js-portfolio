c5dabd3b04db7d571901dae9068c800f38904890
jdmm0718 <juandavidmm0718@gmail.com>
2022-05-01 15:25:56 -0500

    activado el watch

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,8 @@ module.exports = {
     filename: '[name].[contenthash].js',
     assetModuleFilename: 'assets/images/[hash][ext][query]'
   },
+  mode: 'development',
+  watch: true,
   resolve: {
     extensions: ['.js'],
     alias: {
